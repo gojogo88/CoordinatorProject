a7fe34ac6e6aebcd19cf59647586d5246f97261c
Jonathan Go <gojonathan.dev@gmail.com>
2022-07-24 10:12:43 +0200

    Initial Commit

---
+++ CoordinatorProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoordinatorProject
+//
+//  Created by Jonathan Go on 24.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
